e5cc69c7f3c907fec155bb7b70bae7b568d47f05
Mehul Chaudhary <31899373+mehul99@users.noreply.github.com>
2019-03-31 15:56:59 +0530

    Add files via upload

---
+++ last.ino
@@ -0,0 +1,58 @@
+const int alc_senPin = 8 ;
+const int redLED = 2 ;
+const int greenLED = 3 ;
+const int motorPin = 9 ;
+int alc_status = 0;
+
+void alert_on();
+void alert_off();
+
+void alert_on() {
+  Serial.println("Alcohol detection status  : True");
+  if (alc_status == 0) {
+    Serial.println("Cutting off the Engine ....");
+  }
+  digitalWrite(motorPin , HIGH);
+  digitalWrite(greenLED , LOW);
+  digitalWrite(redLED   , HIGH);
+  alc_status = 1;
+  delay(8000);
+
+}
+
+void alert_off() {
+  Serial.println("Alcohol detection status  : False");
+  if (alc_status == 1) {
+    Serial.println("Turning ON the Engine ....");
+  }
+  digitalWrite(motorPin , LOW );
+  digitalWrite(greenLED , HIGH);
+  digitalWrite(redLED   , LOW );
+  alc_status == 0;
+}
+void setup() {
+
+  pinMode(alc_senPin , INPUT );
+  pinMode(motorPin   , OUTPUT);
+  pinMode(greenLED   , OUTPUT);
+  pinMode(redLED     , OUTPUT);
+
+  Serial.begin(9600);
+  Serial.println("Initializing alcohol detection unit ..........");
+
+  digitalWrite(motorPin , LOW );
+  digitalWrite(greenLED , HIGH);
+  digitalWrite(redLED   , LOW );
+
+
+}
+
+void loop() {
+
+  while (alc_senPin == HIGH) {
+    alert_on();
+  }
+
+  alert_off();
+
+}
